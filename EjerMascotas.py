9168027b3684ebae87307d12c16ae45b3058f0c0
ManuelPizarroBustos <ma.pizarrob@duocuc.cl>
2024-07-04 17:19:01 -0400

    Incorporacion de cantidad de caracteres

--- EjerMascotas.py
+++ EjerMascotas.py
@@ -1,10 +1,11 @@
 
 op=1
-while(op!=3):
+while(op!=4):
     op=int(input("""\n**** Veterinaria Colitas Felices ****
     1. Ingresar Mascota
     2. Cantidad de Mascotas
-    3. Salir
+    3. Cantidad de caracteres
+    4. Salir
         Elija Opción: """))
     if(op==1):
         with open("mascotas.txt","a") as f:
@@ -17,7 +18,13 @@ while(op!=3):
         with open("mascotas.txt","r") as f:
             lista=f.readlines()
         print("\n\tLa cantidad de mascotas es "+str(len(lista)))
-    
+    if(op==3):
+        with open("mascotas.txt","r") as f:
+            lista=f.readlines()
+        print(lista)
+        print("Las mascotas registradas son:")
+        for mas in lista:
+            print(mas.strip()+" = "+str(len(mas)-1)+" caracteres")
          
     
     
